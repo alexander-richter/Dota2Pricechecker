dbc0a231fd2afad62dae0e6a44fca69fab37429a
buetz <alexander.m.richter@gmail.com>
2014-02-20 13:34:47 +0100

    first version

---
+++ Dota2Pricechecker/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Dota2Pricechecker
+{
+    partial class Pricechecker
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pricechecker));
+            this.backgroundWorkerCurrency = new System.ComponentModel.BackgroundWorker();
+            this.groupBoxWarehouse = new System.Windows.Forms.GroupBox();
+            this.labelGold = new System.Windows.Forms.Label();
+            this.labelItemname = new System.Windows.Forms.LinkLabel();
+            this.groupBoxMarket = new System.Windows.Forms.GroupBox();
+            this.textBoxMarketQuotes = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crawlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lookUpItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxOptions = new System.Windows.Forms.GroupBox();
+            this.checkBoxTopMost = new System.Windows.Forms.CheckBox();
+            this.backgroundWorkerPricecheck = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBoxWarehouse.SuspendLayout();
+            this.groupBoxMarket.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBoxOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorkerCurrency
+            // 
+            this.backgroundWorkerCurrency.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerCurrency_DoWork);
+            // 
+            // groupBoxWarehouse
+            // 
+            this.groupBoxWarehouse.AutoSize = true;
+            this.groupBoxWarehouse.Controls.Add(this.labelGold);
+            this.groupBoxWarehouse.Controls.Add(this.labelItemname);
+            this.groupBoxWarehouse.Location = new System.Drawing.Point(12, 73);
+            this.groupBoxWarehouse.Name = "groupBoxWarehouse";
+            this.groupBoxWarehouse.Size = new System.Drawing.Size(200, 72);
+            this.groupBoxWarehouse.TabIndex = 0;
+            this.groupBoxWarehouse.TabStop = false;
+            this.groupBoxWarehouse.Text = "Dota 2 Warehouse";
+            // 
+            // labelGold
+            // 
+            this.labelGold.AutoSize = true;
+            this.labelGold.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGold.ForeColor = System.Drawing.Color.Goldenrod;
+            this.labelGold.Location = new System.Drawing.Point(7, 40);
+            this.labelGold.Name = "labelGold";
+            this.labelGold.Size = new System.Drawing.Size(124, 16);
+            this.labelGold.TabIndex = 1;
+            this.labelGold.Text = "-- Gold Amount --";
+            // 
+            // labelItemname
+            // 
+            this.labelItemname.AutoSize = true;
+            this.labelItemname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelItemname.LinkColor = System.Drawing.Color.Black;
+            this.labelItemname.Location = new System.Drawing.Point(7, 20);
+            this.labelItemname.Name = "labelItemname";
+            this.labelItemname.Size = new System.Drawing.Size(110, 16);
+            this.labelItemname.TabIndex = 0;
+            this.labelItemname.TabStop = true;
+            this.labelItemname.Text = "-- Item Name --";
+            this.labelItemname.VisitedLinkColor = System.Drawing.Color.Black;
+            this.labelItemname.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.labelItemname_LinkClicked);
+            // 
+            // groupBoxMarket
+            // 
+            this.groupBoxMarket.Controls.Add(this.textBoxMarketQuotes);
+            this.groupBoxMarket.Location = new System.Drawing.Point(12, 151);
+            this.groupBoxMarket.Name = "groupBoxMarket";
+            this.groupBoxMarket.Size = new System.Drawing.Size(131, 223);
+            this.groupBoxMarket.TabIndex = 1;
+            this.groupBoxMarket.TabStop = false;
+            this.groupBoxMarket.Text = "Dota 2 Marketplace";
+            // 
+            // textBoxMarketQuotes
+            // 
+            this.textBoxMarketQuotes.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBoxMarketQuotes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxMarketQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMarketQuotes.Location = new System.Drawing.Point(8, 20);
+            this.textBoxMarketQuotes.Multiline = true;
+            this.textBoxMarketQuotes.Name = "textBoxMarketQuotes";
+            this.textBoxMarketQuotes.ReadOnly = true;
+            this.textBoxMarketQuotes.Size = new System.Drawing.Size(115, 197);
+            this.textBoxMarketQuotes.TabIndex = 0;
+            this.textBoxMarketQuotes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(221, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataToolStripMenuItem
+            // 
+            this.dataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crawlerToolStripMenuItem,
+            this.lookUpItemToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.dataToolStripMenuItem.Text = "Data";
+            // 
+            // crawlerToolStripMenuItem
+            // 
+            this.crawlerToolStripMenuItem.Name = "crawlerToolStripMenuItem";
+            this.crawlerToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.crawlerToolStripMenuItem.Text = "Crawler";
+            // 
+            // lookUpItemToolStripMenuItem
+            // 
+            this.lookUpItemToolStripMenuItem.Name = "lookUpItemToolStripMenuItem";
+            this.lookUpItemToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.lookUpItemToolStripMenuItem.Text = "Item Lookup";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(138, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // groupBoxOptions
+            // 
+            this.groupBoxOptions.Controls.Add(this.checkBoxTopMost);
+            this.groupBoxOptions.Location = new System.Drawing.Point(12, 28);
+            this.groupBoxOptions.Name = "groupBoxOptions";
+            this.groupBoxOptions.Size = new System.Drawing.Size(200, 39);
+            this.groupBoxOptions.TabIndex = 3;
+            this.groupBoxOptions.TabStop = false;
+            this.groupBoxOptions.Text = "Options";
+            // 
+            // checkBoxTopMost
+            // 
+            this.checkBoxTopMost.AutoSize = true;
+            this.checkBoxTopMost.Location = new System.Drawing.Point(10, 16);
+            this.checkBoxTopMost.Name = "checkBoxTopMost";
+            this.checkBoxTopMost.Size = new System.Drawing.Size(71, 17);
+            this.checkBoxTopMost.TabIndex = 0;
+            this.checkBoxTopMost.Text = "Top Most";
+            this.checkBoxTopMost.UseVisualStyleBackColor = true;
+            this.checkBoxTopMost.CheckedChanged += new System.EventHandler(this.checkBoxTopMost_CheckedChanged);
+            // 
+            // backgroundWorkerPricecheck
+            // 
+            this.backgroundWorkerPricecheck.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerPricecheck_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Pricechecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 386);
+            this.Controls.Add(this.groupBoxOptions);
+            this.Controls.Add(this.groupBoxMarket);
+            this.Controls.Add(this.groupBoxWarehouse);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Pricechecker";
+            this.Text = "Dota 2 Pricechecker";
+            this.groupBoxWarehouse.ResumeLayout(false);
+            this.groupBoxWarehouse.PerformLayout();
+            this.groupBoxMarket.ResumeLayout(false);
+            this.groupBoxMarket.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBoxOptions.ResumeLayout(false);
+            this.groupBoxOptions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorkerCurrency;
+        private System.Windows.Forms.GroupBox groupBoxWarehouse;
+        private System.Windows.Forms.GroupBox groupBoxMarket;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crawlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lookUpItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label labelGold;
+        private System.Windows.Forms.LinkLabel labelItemname;
+        private System.Windows.Forms.TextBox textBoxMarketQuotes;
+        private System.Windows.Forms.GroupBox groupBoxOptions;
+        private System.Windows.Forms.CheckBox checkBoxTopMost;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerPricecheck;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Dota2Pricechecker/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics; //Process.Start ... open browser ...
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net; //web
+using HtmlAgilityPack; //web
+using System.Globalization; //decimal english 0.9239 not 0,234
+using System.Text.RegularExpressions; //Regex
+
+
+namespace Dota2Pricechecker
+{
+    public partial class Pricechecker : Form
+    {
+        public Pricechecker()
+        {
+            InitializeComponent();
+            //add eventlisteners for backgroundworkers at the very beginning
+            backgroundWorkerCurrency.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorkerCurrency_RunWorkerCompleted);
+
+            //start the backgroundworker Currency
+            backgroundWorkerCurrency.RunWorkerAsync();
+        }
+
+        decimal currencyDollar = 0.731m;
+        decimal currencyRus = 0.029m;
+        decimal currencyBr = 0.304m;
+        decimal currencyPound = 1.218m;
+        decimal currencyEuro = 1.00m;
+
+        HtmlWeb web = new HtmlWeb();
+
+        string clipboard;
+        string clipboard_old;
+
+
+        private void backgroundWorkerCurrency_DoWork(object sender, DoWorkEventArgs e)
+        {
+            new WebClient();
+
+            //load currency website to get recent exchange rates
+            HtmlAgilityPack.HtmlDocument document = web.Load("http://www.x-rates.com/table/?from=EUR&amount=1.00");
+            try
+            {
+                //get all currencies from x-rates.com
+                foreach (HtmlNode node in document.DocumentNode.SelectNodes("//td[@class='rtRates']//a[@href]"))
+                {
+                    HtmlAttribute htmlAttribute = node.Attributes["href"];
+
+                    //filter specific currencies (convert & to &amp;)
+                    if (htmlAttribute.Value.ToString() == "/graph/?from=USD&amp;to=EUR")
+                    {
+                        currencyDollar = Convert.ToDecimal(node.InnerText.Trim(), CultureInfo.InvariantCulture);
+                    }
+                    if (htmlAttribute.Value.ToString() == "/graph/?from=RUB&amp;to=EUR")
+                    {
+                        currencyRus = Convert.ToDecimal(node.InnerText.Trim(), CultureInfo.InvariantCulture);
+                    }
+                    if (htmlAttribute.Value.ToString() == "/graph/?from=BRL&amp;to=EUR")
+                    {
+                        currencyBr = Convert.ToDecimal(node.InnerText.Trim(), CultureInfo.InvariantCulture);
+                    }
+                    if (htmlAttribute.Value.ToString() == "/graph/?from=GBP&amp;to=EUR")
+                    {
+                        currencyPound = Convert.ToDecimal(node.InnerText.Trim(), CultureInfo.InvariantCulture);
+                    }
+                }
+                
+            }
+            catch
+            {
+                textBoxMarketQuotes.Text += "ERROR GETTING CURRENCY";
+            }
+        }
+
+        private void backgroundWorkerCurrency_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //start the Timer, when currencies are loaded
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.clipboard = Clipboard.GetText();
+            if (this.clipboard != this.clipboard_old && this.clipboard.Contains("http://www.dota2wh.com/item/") && this.clipboard.StartsWith("http://www.dota2wh.com/item/"))
+            {
+                textBoxMarketQuotes.Clear();
+                textBoxMarketQuotes.Text = "Loading...";
+                labelItemname.Text = "-- loading item... --";
+                labelGold.Text = "-- loading data... --";
+                clipboard_old = clipboard;
+                backgroundWorkerPricecheck.RunWorkerAsync();
+            }
+        }
+
+        string itemname;
+        int gold;
+        decimal currency;
+        decimal quote;
+
+        private void backgroundWorkerPricecheck_DoWork(object sender, DoWorkEventArgs e)
+        {
+            new WebClient();
+
+            HtmlAgilityPack.HtmlDocument document = web.Load(clipboard);
+            try
+            {
+                HtmlNode node = document.DocumentNode.SelectSingleNode("//div[@class='name']");
+                {
+                    itemname = node.InnerText.Trim();
+                }
+            }
+            catch
+            {
+                textBoxMarketQuotes.Text = "could not recieve itemname";
+            }
+
+            try
+            {
+                HtmlNode node = document.DocumentNode.SelectSingleNode("//div[@class='label price']");
+                {
+                    //if (node != null)//if not already sold "not available" (not for sale etc...)
+
+                    //get gold amount calculating the buy quote
+                    gold = Convert.ToInt32(Regex.Match(node.InnerText, @"\d+").Value);
+
+                    labelItemname.Text = itemname.Replace("&#39;", "'");
+                    labelGold.Text = gold + "gold";
+
+                    //look up dota market place...
+                    int try_again_counter = 0;
+                    try_again: //if getting data from market failed... try it again 3 times....
+                        try_again_counter++;
+                        try
+                        {
+                            document = web.Load("http://steamcommunity.com/market/listings/570/" + itemname.Replace(" ", "%20").Replace("&#39;", "%27"));
+                            //get the €_€
+                            textBoxMarketQuotes.Clear();
+                            textBoxMarketQuotes.Text = "\r\n";
+                            foreach (HtmlNode nod1 in document.DocumentNode.SelectNodes("//span[@class='market_listing_price market_listing_price_with_fee']"))
+                            {
+                                if (nod1 != null)
+                                {
+                                    //all currencies...
+                                    if (nod1.InnerText.Contains("USD"))
+                                    {
+                                        currency = currencyDollar;
+                                    }
+                                    else if (nod1.InnerText.Contains("p&#1091;&#1073;."))
+                                    {
+                                        currency = currencyRus;
+                                    }
+                                    else if (nod1.InnerText.Contains("&#163;"))
+                                    {
+                                        currency = currencyPound;
+                                    }
+                                    else if (nod1.InnerText.Contains("&#82;"))
+                                    {
+                                        currency = currencyBr;
+                                    }
+                                    else
+                                    {
+                                        currency = currencyEuro;
+                                    }
+                                    Math.Round(currency, 4, MidpointRounding.ToEven);
+                                        
+                                        
+                                    //price to €€
+                                    decimal price = Math.Round(Convert.ToDecimal(nod1.InnerText.Replace(",", ".").Replace(" ", "").Replace("p&#1091;&#1073;.", "").Replace("&#36;", "").Replace("USD", "").Replace("&#8364;", "").Replace("&#163;", "").Replace("&#82;", "").Replace("-", "0").Trim(), CultureInfo.InvariantCulture) * currency, 2, MidpointRounding.ToEven);
+                                    //calc the buyóut quote
+                                    quote = Math.Round(Convert.ToDecimal((price / gold) * 1000.0m, CultureInfo.InvariantCulture), 2);
+                                        
+                                    textBoxMarketQuotes.AppendText(price + " €" + " (" + quote + ")\r\n");
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            if (try_again_counter <= 3) 
+                            {
+                                textBoxMarketQuotes.Text += "Error getting Market Data ...\r\n Trying again in 1 second.";
+                                System.Threading.Thread.Sleep(1000);
+                                goto try_again;
+                            }
+                            else
+                            {
+                                textBoxMarketQuotes.Text += "No Market Data available.";
+                            }
+                        }
+                }
+            }
+            catch
+            {
+                textBoxMarketQuotes.Text += "Error WH Single Item Site";
+            }
+        }
+
+
+        private void checkBoxTopMost_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxTopMost.Checked)
+            {
+                Dota2Pricechecker.Pricechecker.ActiveForm.TopMost = true;
+            }
+            else
+            {
+                Dota2Pricechecker.Pricechecker.ActiveForm.TopMost = false;
+            }
+        }
+
+        //click itemlink
+        private void labelItemname_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://steamcommunity.com/market/listings/570/" + itemname.Replace(" ", "%20").Replace("&#39;", "%27") + "#searchResultsTable");
+        }
+    }
+}
